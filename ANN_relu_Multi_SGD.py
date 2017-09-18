6e671853e3e0312c9ca5075f951735afd63b1feb
Renzo Morales <renvmorales@gmail.com>
2017-09-18 16:47:35 -0300

    Add files via upload

--- ANN_relu_Multi_SGD.py
+++ ANN_relu_Multi_SGD.py
@@ -1,10 +1,13 @@
-# Stochastic GD optimization for ANN models using 'relu' activation 
-# function for multiclass problems.
+# This API implements the following tasks:
+#  - Multi-layer 'relu' ANN models for multiclass problems
+#  - Stochastic GD optimization
+# 
 # Implementation from scratch (mainly using Numpy).
 import numpy as np
 import matplotlib.pyplot as plt
 from  ann_functions import *
 import time
+
 
 
 class ANN_relu(object):
@@ -17,16 +20,18 @@ class ANN_relu(object):
 
 
 
-	def fit(self, X, Y, alpha=1e-6, reg=1e-4, epochs=5000, 
-		show_fig=False):		
+	def fit(self, X, Y, alpha=1e-3, reg=1e-4, epochs=5000, show_fig=False):		
 		N, D = X.shape
 		K = len(np.unique(Y))
 
 		self.N = N  # this variable will be used for normalization
 		self.D = D  # store the dimension of the training dataset
+		self.K = K  # output dimension
+
 		# stores all hyperparameter values
 		self.hyperparameters = {'alpha':alpha, 'reg':reg, 'epochs':epochs}
 		
+
 		# creates an indicator matrix for the target
 		Trgt = np.zeros((N, K))
 		Trgt[np.arange(N), Y.astype(np.int32)] = 1
@@ -80,21 +85,19 @@ class ANN_relu(object):
 	def forward(self, X):
 		self.Z = [X] # this list contains all hidden unities + input/output
 		for i in range(0,len(self.M)):
-			self.Z.append(forward_step_relu(self.Z[i], self.W[i], self.b[i]))
-		self.Z.append(forward_step(self.Z[len(self.M)], self.W[len(self.M)], self.b[len(self.M)]))
-		self.Z[-1] = softmax(self.Z[-1])
+			self.Z.append(relu(self.Z[i].dot(self.W[i]) + self.b[i]))
+		self.Z.append(softmax(self.Z[len(self.M)].dot(self.W[len(self.M)]) + self.b[len(self.M)]))
 		return self.Z[-1]
 
 
 
 	def back_prop(self, Y, PY, alpha, reg):
-		dZ = PY-Y
+		dZ = (PY-Y)/len(Y)
 		Z = self.Z[:-1]
 		Wbuf = self.W
 		for i in range(1,len(self.W)+1):
 			self.W[-i] -= alpha * (Z[-i].T.dot(dZ) + reg/self.N*self.W[-i])
 			self.b[-i] -= alpha * (dZ.sum(axis=0) + reg/self.N*self.b[-i])
-			# dZ = dZ.dot(Wbuf[-i].T) * 0.5*(1+np.sign(Z[-i]))
 			dZ = dZ.dot(Wbuf[-i].T) * (Z[-i]>0)
 
 
@@ -104,33 +107,46 @@ class ANN_relu(object):
 		return np.argmax(PY, axis=1)
 
 
+
 def main():
 # number of samples for each class
 	N_class = 1000
 
-# generates random 2-D points 
+
+# generate random 2-D points 
 	X1 = np.random.randn(N_class,2)+np.array([2,2])
 	X2 = np.random.randn(N_class,2)+np.array([-2,-2])
 	X3 = np.random.randn(N_class,2)+np.array([-2,2])
 	X4 = np.random.randn(N_class,2)+np.array([2,-2])
 	X = np.vstack([X1,X2,X3,X4])
 
+
 # labels associated to the input
 	Y = np.array([0]*N_class+[1]*N_class+[2]*N_class+[3]*N_class)
 	# Y = np.reshape(Y, (len(Y),1))
+
+
+# general data information for the training process
+	print('Total input samples:',X.shape[0])
+	print('Data dimension:',X.shape[1])
+	print('Number of output classes:',len(np.unique(Y)))
+	print('\n')
 
 
 # scatter plot of original labeled data
 	plt.scatter(X[:,0],X[:,1],c=Y,s=50,alpha=0.5)
 	plt.show()
 
-# creates an ANN model with the specified 4 hidden layers
+
+# create an ANN model with the specified 4 hidden layers
 	model = ANN_relu([10,10,10,10])
 
-# fits the model with the hyperparameters set	
-	model.fit(X, Y, alpha=1e-3, reg=0.9, epochs=8000, show_fig=True)
+
+# fit the model with the hyperparameters set	
+	model.fit(X, Y, alpha=1e-3, reg=0, epochs=5000, show_fig=True)
 	
-# computes the model accuracy	
+
+# compute the model accuracy	
 	Ypred = model.predict(X)
 	print('\nFinal model accuracy: {:.4f}'.format(np.mean(Y==Ypred)))
 
--- fer_SGD.py
+++ fer_SGD.py
@@ -1,18 +1,21 @@
-# Stochastic gradient descend ANN model with 'relu' activation function
-# for the ICML 2013 facial recognition dataset. 
-# P.S: It can take very long time to run depending if the majority of the 
-# dataset is used for training.
+# Stochastic gradient descent used for training a 'relu' ANN model
+# on the ICML 2013 facial recognition dataset. 
+# 
+# P.S: It can take very long time to run depending on the training 
+# dataset size.
 import numpy as np
 import matplotlib.pyplot as plt
 from ANN_relu_Multi_SGD import ANN_relu
-# from sklearn.utils import shuffle
+from sklearn.externals import joblib
+
 
 
 
 def main():
-    npzfile = np.load('fer_sample.npz')
-    X = npzfile['X']
-    Y = npzfile['Y']
+    npzfile = np.load('fer2train5k.npz')
+    X = npzfile['Xtrain']
+    Y = npzfile['Ytrain']
+
 
 
     print('Number of samples:',X.shape[0])
@@ -21,22 +24,24 @@ def main():
     print('\n')
 
 
+# create the ANN model with a specific number of hidden layers/unities
     model = ANN_relu([100,100,100,100])
-    # model = ANN_relu([200,200,200])
-    # model.fit(X, Y, learning_rate=1*10e-7, epochs=10000, reg=0, show_fig=True)
-    model.fit(X, Y, alpha=1e-4, epochs=10000, reg=9e-1, show_fig=True)
-    # # print('The total score is: ', model.score(X, Y))
+
+
+# train the model with a hyperparameters setting
+    model.fit(X, Y, alpha=1e-4, epochs=5000, reg=0, show_fig=True)
+
+
+# compute the prediciton/accuracy based on input data
     Ypred = model.predict(X)
     print('\nFinal model accuracy: {:.4f}'.format(np.mean(Y==Ypred)))
 
 
 
-    # save the model object to a file
-    from sklearn.externals import joblib
+# save the model object to a file
     joblib.dump(model, 'fer_ANN_relu_SGD.sav')
     
-    # import pickle
-    # pickle.dump(model, open('ANN_relu.sav', 'wb'))
+
 
 
 
